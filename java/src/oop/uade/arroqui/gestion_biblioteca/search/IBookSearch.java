ee1e43d1f2dcf66dd307a7d380e2f7c5220fef2c
mquiss <mariaquispegamez@gmail.com>
2025-06-03 01:22:10 -0300

    java(oop): remove access modifier from interface method

--- java/src/oop/uade/arroqui/gestion_biblioteca/search/IBookSearch.java
+++ java/src/oop/uade/arroqui/gestion_biblioteca/search/IBookSearch.java
@@ -5,5 +5,5 @@ import oop.uade.arroqui.gestion_biblioteca.model.Book;
 import java.util.List;
 
 public interface IBookSearch {
-    public List<Book> search(List<Book> bookList);
+    List<Book> search(List<Book> bookList);
 }
