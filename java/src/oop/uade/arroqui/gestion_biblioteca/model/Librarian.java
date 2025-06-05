0f35654e695990c19b41f851915730f37330e9b8
mquiss <mariaquispegamez@gmail.com>
2025-06-04 21:01:04 -0300

    java(oop): add librarian subclass to library management exercise

---
+++ java/src/oop/uade/arroqui/gestion_biblioteca/model/Librarian.java
@@ -0,0 +1,7 @@
+package oop.uade.arroqui.gestion_biblioteca.model;
+
+public class Librarian extends User {
+    public Librarian(String fistName, String lastName, String email, String password) {
+        super(fistName, lastName, email, password);
+    }
+}
