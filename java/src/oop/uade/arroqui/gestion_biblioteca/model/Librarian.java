287b92a70c3501f4fbd5cb0ec121a8d94ac53784
mquiss <mariaquispegamez@gmail.com>
2025-06-06 21:40:37 -0300

    java(oop): delete controller and model

--- java/src/oop/uade/arroqui/gestion_biblioteca/model/Librarian.java
+++
@@ -1,7 +0,0 @@
-package oop.uade.arroqui.gestion_biblioteca.model;
-
-public class Librarian extends User {
-    public Librarian(String fistName, String lastName, String email, String password) {
-        super(fistName, lastName, email, password);
-    }
-}
